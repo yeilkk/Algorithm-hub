579d5e4661493e0394357a788d4836c075f64bac
yeil kim <96576724+yeilkk@users.noreply.github.com>
2024-08-12 00:44:00 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.04 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] solution(String[] keyinput, int[] board) {
+        int width = board[0]/2;
+        int height = board[1]/2;
+        int[] map = new int[]{height, width, height*-1, width*-1};
+
+        int[] arr = new int[2];
+        for(String str : keyinput){
+            if(str.equals("left")){
+                if(arr[0]-1>=map[3])
+                    arr[0]--;
+            }
+            else if(str.equals("right")){
+                if(arr[0]+1<=map[1])
+                    arr[0]++;
+            }
+            else if(str.equals("up")){
+                if(arr[1]+1<=map[0])
+                    arr[1]++;
+            }
+            else{
+                if(arr[1]-1>=map[2])
+                    arr[1]--;
+            }
+        }
+        
+        return arr;
+    }
+}
